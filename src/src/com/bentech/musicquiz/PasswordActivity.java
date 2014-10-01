96a0829c2b3809fab39a49136cae4d128b337407
Ben TheGreat <pokeboygarcia@gmail.com>
2014-10-01 12:10:48 -0700

    Revert "Revert 35c6ec2..95d82a6"
    This reverts commit bcbd677689b67f023c6766447e083bfb8134d19f.

--- src/src/com/bentech/musicquiz/PasswordActivity.java
+++ src/src/com/bentech/musicquiz/PasswordActivity.java
@@ -1,7 +1,15 @@
 package com.bentech.musicquiz;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.github.kevinsawicki.http.*;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -22,6 +30,9 @@ public class PasswordActivity extends Activity {
 		{
 			Code = Code.substring(0, CodeLength);
 		}
+		ProgressBar bar = (ProgressBar) v.findViewById(R.id.progressBar1);
+		bar.setProgress(CodeLength);
+		v.invalidate();
 	}
 	
 	public void Key0(View v)
@@ -85,5 +96,46 @@ public class PasswordActivity extends Activity {
 		ProgressBar bar = (ProgressBar) v.findViewById(R.id.progressBar1);
 		bar.setProgress(CodeLength);
 		v.invalidate();
+	}
+	
+	private boolean TestCode()
+	{
+		try
+		{
+			URL url = new URL("http://epicabsol.us.to:5041/auth-" + Code);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			String result = "";
+			
+			result = br.readLine();
+			if (result == "good")
+			{
+				Log.d("ServerResult", "Good!!!!!");
+			}
+			else if (result == "bad")
+			{
+				Log.d("ServerResult", "BAD!!!!");
+			}
+			else
+			{
+				Log.d("ServerResult", result);
+			}
+			return true;
+		}
+		catch (Exception ex)
+		{
+			return false;
+		}
+	}
+	public void SubmitCode(View v)
+	{
+		if (TestCode())
+		{
+			Intent HomeIntent = new Intent(this, HomeActivity.class);
+	    	startActivity(HomeIntent);
+		}
+		else
+		{
+			BackPassword(v);
+		}
 	}
 }
